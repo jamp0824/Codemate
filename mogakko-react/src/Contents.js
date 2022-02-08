9766be05129f96cdfe6a5ff51d982b5e5f6f77ac
jun onAn <loneyjune@naver.com>
2022-02-08 13:40:11 +0900

    day 3 for mogakko

--- mogakko-react/src/Contents.js
+++ mogakko-react/src/Contents.js
@@ -1,6 +1,7 @@
+import style from "./Contents.module.css";
 function Contents() {
   return (
-    <>
+    <div className={style.description}>
       <p>
         인스타그램(Instagram)은 메타에서 운영하고 있는 이미지 공유 중심의 미국의
         소셜 미디어이다. iOS, 안드로이드, 윈도우 10, 그리고 웹에서 서비스되고
@@ -11,7 +12,7 @@ function Contents() {
         <br />
         영어,스페인어, 프랑스어, 일본어, 한국어 등 25개의 언어를 지원한다.
       </p>
-    </>
+    </div>
   );
 }
 export default Contents;
